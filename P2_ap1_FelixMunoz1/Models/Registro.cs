66a498e7bedafe654a7de452b407803faf4eb65e
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-10-27 19:37:17 -0400

    Trabajando Model

---
+++ P2_ap1_FelixMunoz1/Models/Registro.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_ap1_FelixMunoz.Models;
+
+public class Registro
+{
+    [Required(ErrorMessage = "Por favor digitar la fecha.")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+}
