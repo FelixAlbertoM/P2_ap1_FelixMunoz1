60b5b55335c763385577bf242c727f2dff0b86bf
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-10-27 20:32:24 -0400

    Service

---
+++ P2_ap1_FelixMunoz1/Service/RegistroService.cs
@@ -0,0 +1,23 @@
+﻿using P2_ap1_FelixMunoz1.DAL;
+
+namespace P2_ap1_FelixMunoz1.Service;
+public class RegistroService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Guardar(Registros registro)
+    {
+        if (!await Existe(registro.Id))
+        {
+            return await Insertar(registro);
+        }
+        else
+        {
+            return await Modificar(registro);
+        }
+    }
+    public async Task<List<Registros>> Listar(Expression<Func<Registros, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registros.Where(criterio).AsNoTracking().ToListAsync();
+    }
+
+}
