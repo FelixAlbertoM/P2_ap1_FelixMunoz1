7b8f05a658dc10739400cf09f097938e8a46fb99
FelixAlbertoM <af301509@gmail.com>
2025-10-29 18:09:35 -0400

    Cambios para el parcial

--- P2_ap1_FelixMunoz1/Models/Registro.cs
+++ P2_ap1_FelixMunoz1/Models/Registro.cs
@@ -4,6 +4,10 @@ namespace P2_ap1_FelixMunoz.Models;
 
 public class Registro
 {
+    [Key]
+    public int IdRegistro { get; set; }
+
+
     [Required(ErrorMessage = "Por favor digitar la fecha.")]
     public DateTime Fecha { get; set; } = DateTime.Now;
 }
--- P2_ap1_FelixMunoz1/Service/RegistroService.cs
+++ P2_ap1_FelixMunoz1/Service/RegistroService.cs
@@ -1,11 +1,14 @@
-﻿using P2_ap1_FelixMunoz1.DAL;
+﻿using Microsoft.EntityFrameworkCore;
+using P2_ap1_FelixMunoz.Models;
+using P2_ap1_FelixMunoz1.DAL;
+using System.Linq.Expressions;
 
 namespace P2_ap1_FelixMunoz1.Service;
 public class RegistroService(IDbContextFactory<Contexto> DbFactory)
 {
-    public async Task<bool> Guardar(Registros registro)
+   /* public async Task<bool> Guardar(Registro registro)
     {
-        if (!await Existe(registro.Id))
+        if (!await Existe(registro.IdRegistro))
         {
             return await Insertar(registro);
         }
@@ -13,11 +16,11 @@ public class RegistroService(IDbContextFactory<Contexto> DbFactory)
         {
             return await Modificar(registro);
         }
-    }
-    public async Task<List<Registros>> Listar(Expression<Func<Registros, bool>> criterio)
+    }*/
+    public async Task<List<Registro>> Listar(Expression<Func<Registro, bool>> criterio)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
-        return await contexto.Registros.Where(criterio).AsNoTracking().ToListAsync();
+        return await contexto.Registro.Where(criterio).AsNoTracking().ToListAsync();
     }
 
 }
